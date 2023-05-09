f6df2b04b8df6e6a2d3970fbe728e5e97e006ad6
Mensagem <angelomorgado2016@gmail.com>
2023-05-09 10:36:57 +0100

    Adicionado teste para verificar se a aplicação consegue abrir/criar a base de dados

--- app/src/main/java/com/example/carros/BdCarrosOpenHelper.kt
+++ app/src/main/java/com/example/carros/BdCarrosOpenHelper.kt
@@ -4,20 +4,22 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-private const val NOME_BASE_DADOS="Carros.db"
 private const val VERSAO_BASE_DADOS = 1
 
 class BdCarrosOpenHelper(
     context: Context?,
-    factory: SQLiteDatabase.CursorFactory?,
 ) : SQLiteOpenHelper(context, NOME_BASE_DADOS, null, VERSAO_BASE_DADOS) {
     override fun onCreate(db: SQLiteDatabase?) {
         requireNotNull(db)
-        TabelaTipoDeMarcas(db!!).cria()
-        TabelaCarros(db!!).cria()
+        TabelaTipoDeMarcas(db).cria()
+        TabelaCarros(db).cria()
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
 
+    }
+
+    companion object{
+        const val  NOME_BASE_DADOS="Carros.db"
     }
 }
