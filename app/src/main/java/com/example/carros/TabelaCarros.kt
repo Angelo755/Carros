2ec1246d358cabc6cb126ba9d2004df78f022ca1
Mensagem <angelomorgado2016@gmail.com>
2023-05-12 09:17:23 +0100

    Criar a funcao para converter uma receita em content value

--- app/src/main/java/com/example/carros/TabelaCarros.kt
+++ app/src/main/java/com/example/carros/TabelaCarros.kt
@@ -6,9 +6,12 @@ import android.provider.BaseColumns
 
 class TabelaCarros(db: SQLiteDatabase):TabelaBd(db, NOME_TABELA) {
     override fun cria() {
-        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, nome TEXT NOT NULL, descrição TEXT, id_TipoDeMarcas INTEGER NOT NULL, FOREIGN KEY(id_TipoDeMarcas) REFERENCES ${TabelaTipoDeMarcas.NOME_TABELA}(${BaseColumns._ID}))")
+        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, $CAMPO_NOME TEXT NOT NULL, $CAMPO_DESCRICAO TEXT, $CAMPO_FK_IDMARCA INTEGER NOT NULL, FOREIGN KEY(id_TipoDeMarcas) REFERENCES ${TabelaTipoDeMarcas.NOME_TABELA}(${BaseColumns._ID}))")
     }
     companion object{
         private const val NOME_TABELA = "Carros"
+        const val CAMPO_NOME="nome"
+        const val CAMPO_DESCRICAO="descricao"
+        const val CAMPO_FK_IDMARCA="id_TipoDeMarca"
     }
 }
--- app/src/main/java/com/example/carros/carros.kt
+++ app/src/main/java/com/example/carros/carros.kt
@@ -1,9 +1,20 @@
 package com.example.carros
 
+import android.content.ContentValues
+
 data class carros (
     var id_TipoDeMarcas: Int,
     var descricao:String,
     var nome:String,
     var ano: Long,
     var id: Long = -1){
+    fun toContentValues(){
+        val valores = ContentValues()
+
+        valores.put(TabelaCarros.CAMPO_NOME,nome)
+        valores.put(TabelaCarros.CAMPO_DESCRICAO,descricao)
+        valores.put(TabelaCarros.CAMPO_FK_IDMARCA,id_TipoDeMarcas)
+
+
+    }
 }
