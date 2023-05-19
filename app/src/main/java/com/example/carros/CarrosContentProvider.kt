f5e894188c5b5a922e4ed46f4276bb1e3dc6f210
Mensagem <angelomorgado2016@gmail.com>
2023-05-19 10:17:15 +0100

    Criar UriMatcher para validar os endereços do content provider

--- app/src/main/java/com/example/carros/CarrosContentProvider.kt
+++ app/src/main/java/com/example/carros/CarrosContentProvider.kt
@@ -2,6 +2,7 @@ package com.example.carros
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
 
@@ -9,6 +10,7 @@ class CarrosContentProvider : ContentProvider(){
     private var bdOpenHelper : BdCarrosOpenHelper?=null
     override fun onCreate(): Boolean {
         bdOpenHelper = BdCarrosOpenHelper(context)
+        return true
     }
 
     override fun query(
@@ -35,6 +37,26 @@ class CarrosContentProvider : ContentProvider(){
 
     override fun update(p0: Uri, p1: ContentValues?, p2: String?, p3: Array<out String>?): Int {
         TODO("Not yet implemented")
+    }
+
+    companion object{
+        private const val AUTORIDADE = "com.example.carros"
+
+        const val TIPOSDEMARCAS = "TiposDeMarcas"
+        const val CARROS = "Carros"
+
+        private const val Uri_TIPOSDEMARCAS = 100
+        private const val Uri_CARROS = 200
+
+
+        fun uriMatcher()= UriMatcher(UriMatcher.NO_MATCH).apply {
+            addURI(AUTORIDADE, TIPOSDEMARCAS, Uri_TIPOSDEMARCAS)
+            addURI(AUTORIDADE, CARROS, Uri_CARROS)
+
+            /*
+            content: //com.example.carros/TIPOSDEMARCAS
+             */
+        }
     }
 
 }
