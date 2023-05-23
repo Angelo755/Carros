f03b9e581feb32fbab67698ad133750d152a0a48
Mensagem <angelomorgado2016@gmail.com>
2023-05-23 09:51:24 +0100

    Implementar o método query do content provider

--- app/src/main/java/com/example/carros/CarrosContentProvider.kt
+++ app/src/main/java/com/example/carros/CarrosContentProvider.kt
@@ -5,6 +5,7 @@ import android.content.ContentValues
 import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
+import android.provider.BaseColumns
 
 class CarrosContentProvider : ContentProvider(){
     private var bdOpenHelper : BdCarrosOpenHelper?=null
@@ -14,28 +15,53 @@ class CarrosContentProvider : ContentProvider(){
     }
 
     override fun query(
-        p0: Uri,
-        p1: Array<out String>?,
-        p2: String?,
-        p3: Array<out String>?,
-        p4: String?
+        uri: Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        selectArgs: Array<out String>?,
+        sortOrder: String?
     ): Cursor? {
+        val bd = bdOpenHelper!!.readableDatabase
+
+        val endereco= uriMatcher().match(uri)
+
+        val tabela = when (endereco){
+            Uri_TIPOSDEMARCAS, Uri_TIPODEMARCAS_ID ->TabelaTipoDeMarcas(bd)
+            Uri_CARROS, Uri_CARROS_ID ->TabelaCarros(bd)
+            else -> null
+        }
+
+        val id = uri.lastPathSegment
+
+        val (selecao, argsSel) = when (endereco){
+            Uri_TIPODEMARCAS_ID, Uri_CARROS_ID -> Pair("${BaseColumns._ID}", arrayOf(id))
+            else -> Pair(selection,selectArgs)
+        }
+
+
+
+        return tabela?.cosulta(
+            projection as Array<String>,
+            selection,
+            selectArgs as Array<String>?,
+            null,
+            null,
+            sortOrder)
+    }
+
+    override fun getType(uri: Uri): String? {
         TODO("Not yet implemented")
     }
 
-    override fun getType(p0: Uri): String? {
+    override fun insert(uri: Uri, projection: ContentValues?): Uri? {
         TODO("Not yet implemented")
     }
 
-    override fun insert(p0: Uri, p1: ContentValues?): Uri? {
+    override fun delete(uri: Uri, projection: String?, selection: Array<out String>?): Int {
         TODO("Not yet implemented")
     }
 
-    override fun delete(p0: Uri, p1: String?, p2: Array<out String>?): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun update(p0: Uri, p1: ContentValues?, p2: String?, p3: Array<out String>?): Int {
+    override fun update(uri: Uri, projection: ContentValues?, selection: String?, selectArgs: Array<out String>?): Int {
         TODO("Not yet implemented")
     }
 
@@ -46,12 +72,17 @@ class CarrosContentProvider : ContentProvider(){
         const val CARROS = "Carros"
 
         private const val Uri_TIPOSDEMARCAS = 100
+        private const val Uri_TIPODEMARCAS_ID = 101
         private const val Uri_CARROS = 200
+        private const val Uri_CARROS_ID = 201
 
 
         fun uriMatcher()= UriMatcher(UriMatcher.NO_MATCH).apply {
             addURI(AUTORIDADE, TIPOSDEMARCAS, Uri_TIPOSDEMARCAS)
+            addURI(AUTORIDADE, "$TIPOSDEMARCAS/#", Uri_TIPODEMARCAS_ID)
             addURI(AUTORIDADE, CARROS, Uri_CARROS)
+            addURI(AUTORIDADE, "$CARROS/#", Uri_CARROS_ID)
+
 
             /*
             content: //com.example.carros/TIPOSDEMARCAS
