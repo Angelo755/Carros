e7870ca63c0b3ba3211ea5f5a380cd27f50fe1e7
Mensagem <angelomorgado2016@gmail.com>
2023-05-26 09:18:15 +0100

    Criar adaptador de carros

---
+++ app/src/main/java/com/example/carros/AdapterCarros.kt
@@ -0,0 +1,25 @@
+package com.example.carros
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class AdapterCarros: RecyclerView.Adapter<AdapterCarros.ViewHolderCarro>() {
+    inner class ViewHolderCarro(itemView: View) : ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderCarro {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolderCarro, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
