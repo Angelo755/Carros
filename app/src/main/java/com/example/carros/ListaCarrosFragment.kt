b106ca6008c5cb8ee29bd5c8bd79f659d8642e9c
Mensagem <angelomorgado2016@gmail.com>
2023-06-06 09:46:18 +0100

    Mostar o menu diferente de acordo com o fragment

--- app/src/main/java/com/example/carros/ListaCarrosFragment.kt
+++ app/src/main/java/com/example/carros/ListaCarrosFragment.kt
@@ -45,6 +45,9 @@ class ListaCarrosFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
 
         val loader=LoaderManager.getInstance(this)
         loader.initLoader(ID_LOADER_CARROS, null, this)
+
+        val activity = activity as MainActivity
+        activity.idMenuAtual = R.menu.menu_lista_carros
     }
 
 
--- app/src/main/java/com/example/carros/MenuPrincipalFragment.kt
+++ app/src/main/java/com/example/carros/MenuPrincipalFragment.kt
@@ -40,6 +40,8 @@ class MenuPrincipalFragment : Fragment() {
             findNavController().navigate(R.id.action_MenuPrincipalFragment_to_ListaCarrosFragment)
         }
 
+        val activity = activity as MainActivity
+        activity.idMenuAtual = R.menu.menu_main
     }
 
     override fun onDestroyView() {
